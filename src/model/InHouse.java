409b8b0a381ef7ea629c9eb1db2763d100e6e677
jorgeperez246 <jorgeperez246.jp@gmail.com>
2022-11-09 14:27:47 -0500

    created Inhouse and Outsourced files

---
+++ src/model/InHouse.java
@@ -0,0 +1,27 @@
+package model;
+
+public class InHouse extends Part {
+
+    private int machineId;
+
+    public InHouse(int id, String name, double price, int stock, int min, int max, int machineId) {
+        super(id, name, price, stock, min, max);
+        this.machineId = machineId;
+    }
+
+    /**
+     * machineid getter
+     * @return machineId
+     * */
+    public int getMachineId() {
+        return machineId;
+    }
+    /**
+     * machineid setter
+     * @param machineId
+     * */
+    public void setMachineId(int machineId) {
+        this.machineId = machineId;
+    }
+
+}
